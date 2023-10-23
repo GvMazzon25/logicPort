dfbfe0c6ee28d8930a5c693ab121c3f6457dde50
GvMazzon25 <gvmazzon25@gmail.com>
2023-10-23 21:01:23 +0200

    not port correction

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def main():
     x = input('Inserisci un numero binario (0 o 1): ')
     result = porta_not(x)
     if result is not None:
-        print(f"NOT di {x} è {result}")
+        print(f"Risultato: {result}")
 
 
 if __name__ == "__main__":
     main()
+
